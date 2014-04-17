540d92476c599327820a78e1cdb5b8e226fc20b5
Dustin Getz <dgetz@wingspan.com>
2014-04-17 18:22:05 -0400

    merge from react

---
+++ js/merge.js
@@ -0,0 +1,38 @@
+define([
+    'lodash'
+], function (_) {
+    'use strict';
+
+    // These two functions are from Facebook React.
+
+
+    function mergeInto(one, two) {
+        //checkMergeObjectArg(one);
+        if (two != null) {
+            //checkMergeObjectArg(two);
+            for (var key in two) {
+                if (!two.hasOwnProperty(key)) {
+                    continue;
+                }
+                one[key] = two[key];
+            }
+        }
+    }
+
+    /**
+     * Shallow merges two structures into a return value, without mutating either.
+     *
+     * @param {?object} one Optional object with properties to merge from.
+     * @param {?object} two Optional object with properties to merge from.
+     * @return {object} The shallow extension of one by two.
+     */
+    var merge = function(one, two) {
+        var result = {};
+        mergeInto(result, one);
+        mergeInto(result, two);
+        return result;
+    };
+
+
+    return merge;
+});
